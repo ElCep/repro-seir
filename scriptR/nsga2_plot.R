3f6bfadbdde2364c99e7a1860345813fc416c7f5
Etienne Delay <etienne.delay@cirad.fr>
2022-11-04 15:26:12 +0000

    re-run graph nsga2

--- scriptR/nsga2_plot.R
+++ scriptR/nsga2_plot.R
@@ -1,9 +1,10 @@
 library(ggplot2)
 
-data.df <- read.csv("~/github/repro-seir/results_nsga/multi_infected_exposed_population2000.csv")
+data.df <- read.csv("~/github/repro-seir/resultats_nsga2/population1300.csv")
+data.df$densityInit <- data.df$popInit/1024 
 
 ggplot(data = data.df)+
-  geom_point(aes(x = objective.integraleExposed, y = objective.integraleInfected, size = evolution.samples))+
+  geom_point(aes(x = objective.integraleExposed, y = objective.integraleInfected, size = evolution.samples, colour = densityInit))+
   # geom_segment(aes(x = 1.5, y = 20, xend = 1, yend = 9.2),
   #              arrow = arrow(length = unit(0.5, "cm")))+
   # geom_segment(aes(x = 22, y = 20, xend = 21.1, yend = 12.2),
@@ -11,28 +12,30 @@ ggplot(data = data.df)+
   # annotate(geom="text", x=1.5, y=21, label="1")+
   # annotate(geom="text", x=22, y=21, label="2")+
   theme_bw()+
-  labs(title = "2000 evolutions for NSGA2", 
+  labs(title = "1300 evolutions for NSGA2", 
        subtitle = "multi-obj : infected and exposed",
        x = paste0("\u2211","\u0394","Exposed"),
-       y = paste0("\u2211","\u0394","Infected"))
+       y = paste0("\u2211","\u0394","Infected"),
+       size = "samples", 
+       colour = "density")
 
-ggsave("~/github/repro-seir/img/nsga2_evol2000.png", width = 10, height = 5)
+ggsave("~/github/repro-seir/img/nsga2_evol1300.png", width = 10, height = 5)
 
-ggplot(data = data.df)+
-  geom_point(aes(x = objective.integraleExposed, y = objective.integraleInfected, size = evolution.samples))+
-  geom_segment(aes(x = 35, y = 35, xend = 27, yend = 25),
-              arrow = arrow(length = unit(0.5, "cm")),
-              colour = "grey")+
-  annotate(geom="text", x=36, y=36, label="3")+
-  theme_bw()+
-  labs(title = "2000 evolutions for NSGA2", 
-       subtitle = "multi-obj : infected and exposed",
-       x = paste0("\u2211","\u0394","Exposed"),
-       y = paste0("\u2211","\u0394","Infected"))+
-  xlim(0, 50)+
-  ylim(0,50)
-
-ggsave("~/github/repro-seir/img/nsga2_evol2000_zoom.png", width = 8, height = 5)
-
-
-data.df[34,1:12]
+# ggplot(data = data.df)+
+#   geom_point(aes(x = objective.integraleExposed, y = objective.integraleInfected, size = evolution.samples))+
+#   geom_segment(aes(x = 35, y = 35, xend = 27, yend = 25),
+#               arrow = arrow(length = unit(0.5, "cm")),
+#               colour = "grey")+
+#   annotate(geom="text", x=36, y=36, label="3")+
+#   theme_bw()+
+#   labs(title = "2000 evolutions for NSGA2", 
+#        subtitle = "multi-obj : infected and exposed",
+#        x = paste0("\u2211","\u0394","Exposed"),
+#        y = paste0("\u2211","\u0394","Infected"))+
+#   xlim(0, 50)+
+#   ylim(0,50)
+# 
+# ggsave("~/github/repro-seir/img/nsga2_evol2000_zoom.png", width = 8, height = 5)
+# 
+# 
+# data.df[34,1:12]
