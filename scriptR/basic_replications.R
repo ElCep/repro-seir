f974c26b48f65546a9bf51033fe118cc55fbfafa
Etienne Delay <etienne.delay@cirad.fr>
2022-12-12 10:09:11 +0000

    update script for ploting 3 repliction and create individual files (one for each replication)

--- scriptR/basic_replications.R
+++ scriptR/basic_replications.R
@@ -3,7 +3,7 @@ library(reshape2)
 
 setwd("~/github/repro-seir/")
 
-data.df <- read.csv("results_bs/m0-seir experiment_simple-table.csv", skip = 6)
+data.df <- read.csv("results_bs/m0-seir experiment-table.csv", skip = 6)
 
 data.df %>%
   group_by(X.step.) %>%
@@ -28,7 +28,19 @@ data.m <- melt(sel,id.vars=c("X.step."),
 
 ggplot(data = data.m)+
   geom_smooth(aes(x = X.step., y = value, color = variable))+
-  labs(x = "time", title = "50 replications")+
+  labs(x = "time", title = "30 replications")+
   theme_bw()
 
 ggsave("img/replication.png", width = 8)
+
+## create 30 file one per sim
+nb <- unique(data.df$X.run.number.)
+
+for(i in nb){
+  sel <- data.df$X.run.number.== i
+  small.df <- data.df[sel,c(10:13)]
+  small.df <- small.df[,c(2,3,1,4)]
+  write_csv(x = small.df, file = paste0("results_bs/simu_indiv/result",i,".csv"), col_names = FALSE )
+}
+
+
